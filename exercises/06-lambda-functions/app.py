ec83c3ff671d61baac589e2bb2b3c638bdde83bc
Daniel Ipince <ipince22@gmail.com>
2021-02-23 01:01:09 +0000

    exercises

--- exercises/06-lambda-functions/app.py
+++ exercises/06-lambda-functions/app.py
@@ -1,2 +1,3 @@
-# your function here
-
+# this function return True if a number is odd.
+is_odd = lambda numero: numero%2 != 0
+print(is_odd(5))
--- exercises/07-lambda-function-two/app.py
+++ exercises/07-lambda-function-two/app.py
@@ -1,5 +1,3 @@
-
-
-
 # From this line above, plese do not change code below
+rapid = lambda cadena: cadena[:-1]
 print(rapid("bob")) #should print bo
